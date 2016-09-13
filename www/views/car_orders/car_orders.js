de409ff940c8ac657f9d9b8d230adcc50d7dc849
danding <1291248710@qq.com>
2016-09-13 20:17:16 +0800

    xx

--- www/views/car_orders/car_orders.js
+++ www/views/car_orders/car_orders.js
@@ -1,15 +1,11 @@
 angular.module('starter')
 
-  .controller('carOrdersController',function($scope,$state,$http, $location,$stateParams){
+  .controller('carOrdersController',function($scope,$state,$http, $location,$rootScope){
 
-    //车险订单  1.简要:carNum,被投保人
+    //车险订单  0.报价中;1.已生成;2.待支付
+    $scope.tabIndex=1;
 
-    $scope.orders=[
-      {name:"ABC",carNum:"鲁A00003",sum:200},
-      {name:"ABC",carNum:"鲁A00003",sum:200},
-      {name:"ABC",carNum:"鲁A00003",sum:200}
-                   ];//车险选好公司后,生成的订单
-    $scope.selected=JSON.parse($stateParams.selected);
+    $scope.orders=$rootScope.car_orders;
 
     $scope.goto=function(url){
       $location.path(url);
@@ -19,9 +15,18 @@ angular.module('starter')
       window.history.back();
     }
 
+    $scope.tab_change=function(i)
+    {
+      $scope.tabIndex=i;
+    }
+
     $scope.setDetail=function(order){
       order.detail=!order.detail;
     }
 
+    $scope.goDetail=function(order)
+    {
+      $state.go('car_order_detail',{order:JSON.stringify(order)});
+    }
 
   });
