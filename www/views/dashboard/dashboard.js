988a89158888e5e870ff3c36e1c25cc309dea4b7
apple-1 <651090371@qq.com>
2016-10-18 18:14:51 +0800

    1

--- www/views/dashboard/dashboard.js
+++ www/views/dashboard/dashboard.js
@@ -145,6 +145,7 @@ angular.module('starter')
         if(json.re==1) {
           var carInfo=json.data[0];
           $scope.carInfo=carInfo;
+          alert('carId='+$scope.carInfo.carId)
         }
         console.log('success');
       })
