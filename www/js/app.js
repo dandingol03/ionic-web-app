66002e31739e0d833783f04d10f0c26e5433b6d2
apple-1 <651090371@qq.com>
2016-10-20 09:10:44 +0800

    1

--- www/js/app.js
+++ www/js/app.js
@@ -694,7 +694,7 @@ angular.module('starter', ['ionic', 'ngCordova','ngBaiduMap','ionic-datepicker',
       local:function(){
         if(window.cordova!==undefined&&window.cordova!==null)
 
-          return "http://211.87.225.197:3000";
+          return "http://211.87.225.195:3000";
 
         else
           return "/proxy/node_server";
